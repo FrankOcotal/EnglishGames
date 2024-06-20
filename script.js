c248d6748e1dfe59574f784a520b510c8f4f7148
FrankOcotal <d54543w@gmail.com>
2024-06-20 05:10:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var incorrectCountElem = document.getElementById('incorrectCount');
 var levelElem = document.getElementById('level');
 
 // Crear un objeto de audio para los aplausos y precargarlo
-var applauseAudio = new Audio('applause.mp3');
+var applauseAudio = new Audio('win.wav');
 
 function updateScores() {
   correctCountElem.textContent = 'Correct: ' + correctCount;
