e8c6095585c6fee7546af0492aa3baceff214569
FrankOcotal <d54543w@gmail.com>
2024-06-26 16:53:35 +0200

    Add files via upload

---
+++ phrases.js
@@ -0,0 +1,43 @@
+var levelPhrases = {
+  1: {
+    'yellow': 'yellow.png',
+    'purple': 'purple.png',
+    'green': 'green.png',
+    'blue': 'blue.png',
+    'pink': 'pink.png',
+  },
+  2: {
+    'pencil': 'pencil_1.png',
+    'notebook': 'notebook_1.png',
+    'ruler': 'ruler_1.png',
+    'glue': 'glue_1.png',
+    'eraser': 'eraser_1.png',
+  },
+  3: {
+    'I have a rabbit': 'rabbit.png',
+    'this is a pencil': 'pencil.png',
+    'this is a monkey': 'monkey.png',
+    'this is an ostrich': 'ostrich.png',
+    'this is a tiger': 'tiger.png',
+    'this is a lion': 'lion.png',
+    'this is an owl': 'owl.png',
+  },
+  4: {
+    'she is a student': 'she_student.png',
+    'he is a doctor': 'he_doctor.png',
+    'he is a fireman': 'he_fireman.png',
+    'she is a singer': 'she_singer.png',
+    'she is a dancer': 'she_dancer.png',
+  },
+  5: {
+    'what is your name?': '',
+    'how are you?': '',
+    'where do you live?': '',
+  }
+};
+
+var levelResponses = {
+  'what is your name?': 'my name is ',
+  'how are you?': 'i am fine',
+  'where do you live?': 'i live in '
+};
