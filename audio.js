c49cc8a53b90d3df40c73a2619ce838d5301e87a
FrankOcotal <d54543w@gmail.com>
2024-06-27 16:24:42 +0200

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,7 +1,7 @@
 // audio.js
 // Crear objetos de audio y precargarlos
-var applauseAudio = new Audio('audio/win.wav');
-var errorAudio = new Audio('audio/error.wav');
+var applauseAudio = new Audio('win.wav');
+var errorAudio = new Audio('error.wav');
 
 applauseAudio.preload = 'auto';
 errorAudio.preload = 'auto';
