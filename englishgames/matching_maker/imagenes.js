5c93cb10f3110bc9cbf31475f1a7cb8e0988937f
Francisco Vasquez <d54543w@gmail.com>
2024-07-21 04:06:19 +0200

    Add files via upload

---
+++ englishgames/matching_maker/imagenes.js
@@ -0,0 +1,22 @@
+const imagenes = {
+    'objetos-clase': [
+        'img/../../../img/pencil.png',
+        'img/../../../img/eraser.png',
+		'img/../../../img/crayon.png',
+		'img/../../../img/classroom.png',
+		'img/../../../img/backpack.png',
+		'img/../../../img/glue.png',
+		'img/../../../img/desk.png',
+        // ... más imágenes de objetos de clase
+    ],
+    'miembros-familia': [
+        'img/../../../img/mom.png',
+        'img/../../../img/dad.png',
+		'img/../../../img/sister.png',
+		'img/../../../img/grandpa.png',
+		'img/../../../img/grandma.png',
+		'img/../../../img/sister.png',
+		'img/../../../img/brother.png',
+        // ... más imágenes de miembros de la familia
+    ]
+};
