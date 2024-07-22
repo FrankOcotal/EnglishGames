719de760e246ab4bd9cf0f1245b2c293d8fe3fdb
Francisco Vasquez <d54543w@gmail.com>
2024-07-22 04:08:34 +0200

    Add files via upload

---
+++ englishgames/teacherbot/questions.js
@@ -0,0 +1,22 @@
+// questions.js
+
+const questions = [
+  {
+    question: 'What is the color of the sky?',
+    answers: ['Blue', 'Green', 'Red'],
+    correctAnswer: 'Blue'
+  },
+  {
+    question: 'What is 2 + 2?',
+    answers: ['3', '4', '5'],
+    correctAnswer: '4'
+  },
+  {
+    question: 'What is the capital of France?',
+    answers: ['Berlin', 'Madrid', 'Paris'],
+    correctAnswer: 'Paris'
+  },
+  // Agrega más preguntas según sea necesario
+];
+
+export { questions };
