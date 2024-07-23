85b1cc9c5271fef9824b7564d8553e1c59839b7e
Francisco Vasquez <d54543w@gmail.com>
2024-07-23 17:30:18 +0200

    Update questions.js

--- englishgames/teacherbot/questions.js
+++ englishgames/teacherbot/questions.js
@@ -1,22 +1,21 @@
-// questions.js
-
-const questions = [
+export const questions = [
   {
-    question: 'What is the color of the sky?',
-    answers: ['Blue', 'Green', 'Red'],
-    correctAnswer: 'Blue'
+    question: "I have a pencil?",
+    answers: [
+      '../../img/pencil.png', 
+      '../../img/eraser.png', 
+      '../../img/classroom.png'
+    ],
+    correctAnswer: 'images/apple.png'
   },
   {
-    question: 'What is 2 + 2?',
-    answers: ['3', '4', '5'],
-    correctAnswer: '4'
-  },
-  {
-    question: 'What is the capital of France?',
-    answers: ['Berlin', 'Madrid', 'Paris'],
-    correctAnswer: 'Paris'
-  },
-  // Agrega más preguntas según sea necesario
+    question: "this is my dad?",
+    answers: [
+      '../../img/dad.png', 
+      '../../img/glue.png', 
+      '../../img/dad.png'
+    ],
+    correctAnswer: 'images/banana.png'
+  }
+  // Añade más preguntas según sea necesario
 ];
-
-export { questions };
