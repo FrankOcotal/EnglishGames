63a833a93395a0253beff130d2b02b6262f177c8
Francisco Vasquez <d54543w@gmail.com>
2024-07-23 17:55:36 +0200

    Update questions.js

--- englishgames/teacherbot/questions.js
+++ englishgames/teacherbot/questions.js
@@ -1,21 +1,21 @@
 export const questions = [
   {
-    question: "I have a pencil?",
+    question: "I have a pencil",
     answers: [
       '../../img/pencil.png', 
       '../../img/eraser.png', 
       '../../img/classroom.png'
     ],
-    correctAnswer: 'images/apple.png'
+    correctAnswer: '../../img/pencil.png'
   },
   {
-    question: "this is my dad?",
+    question: "this is my dad",
     answers: [
-      '../../img/dad.png', 
+      '../../img/grandpa.png', 
       '../../img/glue.png', 
       '../../img/dad.png'
     ],
-    correctAnswer: 'images/banana.png'
+    correctAnswer: '../../img/dad.png'
   }
   // Añade más preguntas según sea necesario
 ];
